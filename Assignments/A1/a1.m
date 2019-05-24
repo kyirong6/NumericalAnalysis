ab4e7ffc860809f15348b5afa527edd702dd68dc
Choenden Kyirong <choendenkyirong@d209-087-056-148.wireless.sfu.ca>
2019-05-24 13:34:50 -0700

    slight change to a2

--- Assignments/A1/a1.m
+++ Assignments/A1/a1.m
@@ -4,9 +4,9 @@ p1 = semilogy(x1, y1);
 xlabel('n');
 ylabel('error');
 title('approximation error of e for equal increments of n')
-x2 = power(2, 0:90);
+x2 = power(2, 0:100);
 y2 = arrayfun(@(x) exp(1) - power((1 + (1/x)), x), x2);
-p2 = semilogx(x2,y2);
+p2 = loglog(x2,y2);
 xlabel('n (powers of 2)')
 ylabel('error')
 title('approximation error of e for powers of 2')

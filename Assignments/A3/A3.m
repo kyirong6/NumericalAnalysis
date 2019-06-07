1225f3bc7f04a52337c29eac768cf960e222265a
Choenden Kyirong <choendenkyirong@Choendens-MBP-2.hitronhub.home>
2019-06-07 16:19:13 -0700

    edited A3

--- Assignments/A3/A3.m
+++ Assignments/A3/A3.m
@@ -5,38 +5,38 @@ k_150 = 150;
 expAk = I;
 expAk2 = I;
 expAk3 = I;
-% 
-% % part a (k=50)
-% for n = 1:k_50
-%     expAk = expAk + ((1/factorial(n)) * A.A^n);
-% end
-% imagesc(real(expAk));
-% colormap gray
-% 
-% % part a (k=150)
-% for n = 1:k_150
-%     expAk = expAk + ((1/factorial(n)) * A.A^n);
-% end
-% imagesc(real(expAk));
-% colormap gray
-% 
-% % part b
-% times = [];
-% for n = 5:5:150
-%     tic; 
-%     for i = 1:n
-%         expAk2 = expAk2 + ((1/factorial(i)) * A.A^i);
-%     end
-%     times = [times toc];
-%     expAk2 = I;
-% end
-% 
-% plt = plot(5:5:150, times);
-% xlabel("k iterations");
-% ylabel("computational time");
-% title("Computational time vs K iterations for exp computation");
-% 
-% part c
+
+% part a (k=50)
+for n = 1:k_50
+    expAk = expAk + ((1/factorial(n)) * A.A^n);
+end
+imagesc(real(expAk));
+colormap gray
+
+% part a (k=150)
+for n = 1:k_150
+    expAk = expAk + ((1/factorial(n)) * A.A^n);
+end
+imagesc(real(expAk));
+colormap gray
+
+% part b
+times = [];
+for n = 5:5:150
+    tic; 
+    for i = 1:n
+        expAk2 = expAk2 + ((1/factorial(i)) * A.A^i);
+    end
+    times = [times toc];
+    expAk2 = I;
+end
+
+plt = plot(5:5:150, times);
+xlabel("k iterations");
+ylabel("computational time");
+title("Computational time vs K iterations for exp computation");
+
+part c
 error = [];
 expA = expm(A.A);
 for n = 5:5:150 
@@ -47,7 +47,7 @@ for n = 5:5:150
     expAk3 = I;
 end
 
-% plt = semilogy(5:5:150, error);
-% xlabel("k iterations");
-% ylabel("Computational error");
-% title("Computational error vs K iterations for exp computation");
+plt = semilogy(5:5:150, error);
+xlabel("k iterations");
+ylabel("Computational error");
+title("Computational error vs K iterations for exp computation");
